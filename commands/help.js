1d0d1959603781ec15af16cc3b52ae5fbf3dba62
parvez855 <130054027+parvez855@users.noreply.github.com>
2025-07-07 17:13:59 +0600

    help.js

--- commands/help.js
+++ commands/help.js
@@ -1,11 +1,10 @@
-
-module.exports = {
+export default {
   name: 'help',
   description: 'List of available commands',
   execute(message) {
     message.reply(`📜 **Pori Moni Commands:**\n
-💖 \`/setup\` – Set current channel for AI chat\n
-📚 \`/help\` – Show this help menu\n
+💖 /setup – Set current channel for AI chat\n
+📚 /help – Show this help menu\n
 Try messaging anything in setup channel after /setup 💬`);
   }
 };
