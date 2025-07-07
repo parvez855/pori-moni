dc085022ce07c1fef024974d22e83b3027569f54
parvez855 <130054027+parvez855@users.noreply.github.com>
2025-07-07 17:27:46 +0600

    help.js

--- commands/help.js
+++ commands/help.js
@@ -5,6 +5,10 @@ export default {
     message.reply(`📜 **Pori Moni Commands:**\n
 💖 /setup – Set current channel for AI chat\n
 📚 /help – Show this help menu\n
+😘 /kiss – Send a cute kiss\n
+😄 /mood – Show Pori Moni’s mood\n
+😡 /angry – Show Pori Moni’s ragi mood\n
+🎁 /gift – Send a sweet virtual gift\n
 Try messaging anything in setup channel after /setup 💬`);
   }
 };
