f9f409422758873516aaf1b249345221be7dc4f2
parvez855 <130054027+parvez855@users.noreply.github.com>
2025-07-07 17:44:34 +0600

    help.js

--- commands/help.js
+++ commands/help.js
@@ -1,14 +1,14 @@
 export default {
   name: 'help',
-  description: 'List of available commands',
+  description: 'Show available commands',
   execute(message) {
-    message.reply(`📜 **Pori Moni Commands:**\n
-💖 /setup – Set current channel for AI chat\n
-📚 /help – Show this help menu\n
-😘 /kiss – Send a cute kiss\n
-😄 /mood – Show Pori Moni’s mood\n
-😡 /angry – Show Pori Moni’s ragi mood\n
-🎁 /gift – Send a sweet virtual gift\n
+    message.reply(`📜 **Pori Moni Commands:**
+💖 /setup – Set current channel for AI chat
+📚 /help – Show this help menu
+😘 /kiss – Send a cute kiss
+😄 /mood – Show Pori Moni’s mood
+😡 /angry – Show Pori Moni’s ragi mood
+🎁 /gift – Send a sweet virtual gift
 Try messaging anything in setup channel after /setup 💬`);
   }
 };
