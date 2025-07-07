d06346920eb1dd783aa11d30c997301f1219530a
parvez855 <130054027+parvez855@users.noreply.github.com>
2025-07-07 17:27:27 +0600

    setup.js

--- commands/setup.js
+++ commands/setup.js
@@ -1,11 +1,9 @@
-
-const SetupChannel = require('../models/SetupChannel');
-
-module.exports = {
+export default {
   name: 'setup',
   description: 'Set current channel for AI replies',
   async execute(message) {
     if (!message.member.permissions.has('Administrator')) return message.reply('🔒 You need admin permission!');
+    const SetupChannel = (await import('../models/SetupChannel.js')).default;
     await SetupChannel.findOneAndUpdate(
       { guildId: message.guildId },
       { channelId: message.channel.id },
